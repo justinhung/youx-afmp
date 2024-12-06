f69aa70f0e121c44227166f5866473bc4c617d4a
Justin Hung <justinhung1994@gmail.com>
2024-12-06 20:38:53 +1100

    feat: added 404 page

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../layouts/Layout";
+import { Box } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <Box sx={{ p: 4, margin: "auto", alignContent: "center" }}>404</Box>
+    </Layout>
+  );
+}
