89ae55eb18835da2b795d330bd0ad263955124e0
Justin Hung <justinhung1994@gmail.com>
2024-12-06 23:48:27 +1100

    feat: set up connection to mongodb and continued with backend setup with cors and routes

--- api/app.js
+++ api/app.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-dotenv.config();
+import "./loadEnvironment.js";
+import express from 'express';
+import cors from 'cors';
+import applications from './routes/applications.js';
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+app.use(cors());
+app.use(express.json());
 
+app.use('/applications', applications);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+// start the Express server
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
---
+++ api/routes/applications.js
@@ -0,0 +1,62 @@
+import express from "express";
+import db from "../db/conn.js";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// Get all applications
+router.get("/applications", async (req, res) => {
+  let collection = await db.collection("applications");
+  let results = await collection.find({})
+    .toArray();
+
+  res.send(results).status(200);
+});
+
+// Get a single application
+router.get("/application/:id", async (req, res) => {
+  let collection = await db.collection("applications");
+  let query = {_id: ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Add a new document to the collection
+router.post("/application", async (req, res) => {
+  let collection = await db.collection("applications");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// Update an application
+router.patch("/application/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = {
+    $push: { comments: req.body }
+  };
+
+  let collection = await db.collection("applications");
+  let result = await collection.updateOne(query, updates);
+
+  res.send(result).status(200);
+});
+
+// Delete applications
+router.delete("/applications", async (req, res) => {
+  const query = {
+    _id: {
+      "$in": req.params.ids.map((id) => ObjectId(id))
+    }
+  };
+
+  const collection = db.collection("applications");
+  let result = await collection.delete(query);
+
+  res.send(result).status(200);
+});
+
+export default router;
