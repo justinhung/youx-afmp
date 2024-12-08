4b65f0d35f7b8f47c12b081bd219a47af18877bc
Justin Hung <justinhung1994@gmail.com>
2024-12-09 01:30:40 +1100

    fix: dist config for amplify deployment

--- frontend/src/pages/Swagger.tsx
+++ frontend/src/pages/Swagger.tsx
@@ -1,12 +1,10 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css'; // Import CSS
 
-const SwaggerUIComponent = () => {
+export default function SwaggerUIComponent() {
   return (
     <div style={{ height: '100vh' }}>
       <SwaggerUI url="/openapi.json" />
     </div>
   );
 };
-
-export default SwaggerUIComponent;
